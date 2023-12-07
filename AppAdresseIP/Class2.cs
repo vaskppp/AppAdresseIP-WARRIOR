dfe78fd2358df970c5b36e3cb6450bb3c494197f
guillaume <guillaumelepostollec@gmail.com>
2023-12-07 09:43:31 +0100

    issou

--- AppAdresseIP/Class2.cs
+++ AppAdresseIP/Class2.cs
@@ -8,7 +8,32 @@ namespace AppAdresseIP
 {
     class Class2
     {
+        //CalculMasque
+        public static int[,] CalculMasque(int CIDR)
+        {
+            int[,] masque = new int[ 4, 8 ];
+            int nombreDe1 = CIDR;
 
-      
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 7; j >= 0; j--)
+                {
+                    if (nombreDe1 > 0)
+                    {
+                        masque[i, j] = 1;
+                        nombreDe1--;
+                    }
+                    else
+                    {
+                        masque[i, j] = 0;
+                    }
+                }
+            }
+
+            return masque;
+        }
     }
 }
+
+
+
